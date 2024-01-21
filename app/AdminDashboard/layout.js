7c36cb5b6ae059200f1b62b7c9282cd6d7897382
Dev-Shakil <shakilhossain1007@gmail.com>
2024-01-21 11:58:11 +0600

    getting back to url salluairservice

--- app/AdminDashboard/layout.js
+++ app/AdminDashboard/layout.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 
-// const getAllPassenger = async () => {
-//   const passenger = await fetch(`/api/passenger`);
-//   return passenger.json();
-// }
+
 const AdminLayout = ({children}) => {
   
   return (
--- app/AdminDashboard/page.js
+++ app/AdminDashboard/page.js
@@ -1,53 +1,37 @@
 
-// import Admin_Table from "../components/Admin_Table";
-// const getAllPassenger = async () => {
-//   try {
-//       const response = await fetch(`${process.env.API_URL}/api/passenger`,{cache:'no-store'});
+import Admin_Table from "../components/Admin_Table";
+const getAllPassenger = async () => {
+  try {
+      const response = await fetch(`${process.env.API_URL}/api/passenger`,{cache:'no-store'});
 
-//       if (!response.ok) {
-//           if (response.status === 404) {
-//               // Handle 404 error, for example, by returning an empty array or showing a user-friendly message.
-//               return [];
-//           }
+      if (!response.ok) {
+          if (response.status === 404) {
+              // Handle 404 error, for example, by returning an empty array or showing a user-friendly message.
+              return [];
+          }
 
-//           throw new Error(`Failed to fetch passengers. Status: ${response.status}`);
-//       }
+          throw new Error(`Failed to fetch passengers. Status: ${response.status}`);
+      }
 
-//       const passengers = await response.json();
-//       return passengers;
-//   } catch (error) {
-//       console.error('Error fetching passengers:', error.message);
-//       throw error;
-//   }
-// };
-// const AdminDashboard = async () => {
+      const passengers = await response.json();
+      return passengers;
+  } catch (error) {
+      console.error('Error fetching passengers:', error.message);
+      throw error;
+  }
+};
+const AdminDashboard = async () => {
   
-//   const passenger = await getAllPassenger(); 
+  const passenger = await getAllPassenger(); 
 
-//   // useLayoutEffect(()=>{
-//   //   const storedUserData = localStorage.getItem('user');
-//   //   const user = storedUserData ? JSON.parse(storedUserData) : null; 
-//   //   if(!user){
-//   //     redirect("/")
-//   //   }
-//   // },[]);
   
  
-//   return (
-//     <div className="m-4 shadow-lg mx-auto w-full">
-//       <Admin_Table passenger={passenger}/>
-//     </div>
-//   );
-// };
-
-// export default AdminDashboard;
-
-import React from 'react'
-
-const AdminDashboard = () => {
   return (
-    <div>AdminDashboard</div>
-  )
-}
+    <div className="m-4 shadow-lg mx-auto w-full">
+      <Admin_Table passenger={passenger}/>
+    </div>
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
+
