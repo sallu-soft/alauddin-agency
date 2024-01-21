a27ac1a2f3a05f18de1a31fd7292456e2c06dda7
Dev-Shakil <shakilhossain1007@gmail.com>
2024-01-21 11:46:06 +0600

    without url pages

--- app/AdminDashboard/page.js
+++ app/AdminDashboard/page.js
@@ -1,50 +1,43 @@
-// "use client"
-// import React, { useLayoutEffect } from "react";
-import { redirect } from "next/navigation";
-import Admin_Table from "../components/Admin_Table";
-const getAllPassenger = async () => {
-  try {
-      const response = await fetch(`${process.env.API_URL}/api/passenger`,{cache:'no-store'});
 
-      if (!response.ok) {
-          if (response.status === 404) {
-              // Handle 404 error, for example, by returning an empty array or showing a user-friendly message.
-              return [];
-          }
+// import Admin_Table from "../components/Admin_Table";
+// const getAllPassenger = async () => {
+//   try {
+//       const response = await fetch(`${process.env.API_URL}/api/passenger`,{cache:'no-store'});
 
-          throw new Error(`Failed to fetch passengers. Status: ${response.status}`);
-      }
+//       if (!response.ok) {
+//           if (response.status === 404) {
+//               // Handle 404 error, for example, by returning an empty array or showing a user-friendly message.
+//               return [];
+//           }
 
-      const passengers = await response.json();
-      return passengers;
-  } catch (error) {
-      console.error('Error fetching passengers:', error.message);
-      throw error;
-  }
-};
-const AdminDashboard = async () => {
-  // console.log("hellow")
-  // const isLoggedIn = typeof window !== "undefined" ? JSON.parse(window.localStorage.getItem('user')) : false;
-  // console.log(isLoggedIn)
-  // if (!isLoggedIn) {
-  //   redirect("/");
-  // }
-  const passenger = await getAllPassenger(); 
+//           throw new Error(`Failed to fetch passengers. Status: ${response.status}`);
+//       }
 
-  // useLayoutEffect(()=>{
-  //   const storedUserData = localStorage.getItem('user');
-  //   const user = storedUserData ? JSON.parse(storedUserData) : null; 
-  //   if(!user){
-  //     redirect("/")
-  //   }
-  // },[]);
+//       const passengers = await response.json();
+//       return passengers;
+//   } catch (error) {
+//       console.error('Error fetching passengers:', error.message);
+//       throw error;
+//   }
+// };
+// const AdminDashboard = async () => {
+  
+//   const passenger = await getAllPassenger(); 
+
+//   // useLayoutEffect(()=>{
+//   //   const storedUserData = localStorage.getItem('user');
+//   //   const user = storedUserData ? JSON.parse(storedUserData) : null; 
+//   //   if(!user){
+//   //     redirect("/")
+//   //   }
+//   // },[]);
   
  
-  return (
-    <div className="m-4 shadow-lg mx-auto w-full">
-      <Admin_Table passenger={passenger}/>
-    </div>
-  );
-};
+//   return (
+//     <div className="m-4 shadow-lg mx-auto w-full">
+//       <Admin_Table passenger={passenger}/>
+//     </div>
+//   );
+// };
 
-export default AdminDashboard;
+// export default AdminDashboard;
